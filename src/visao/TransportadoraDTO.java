17e702ee059470d998ea55d93b01361cce6d04b2
Arthur-Henrique <Arthur.Henrique@esus.ufsc.br>
2016-05-24 05:44:54 -0300

    Apresentacao das estatisticas

--- src/visao/TransportadoraDTO.java
+++ src/visao/TransportadoraDTO.java
@@ -1,18 +1,37 @@
 package visao;
 
 public class TransportadoraDTO{
-	public final int referenciaTemporal;
+	public int referenciaTemporal = 0;
 	
 	public final String[] cabecalhoLinhaDoTempo = new String[]{"Momento", "Caminhao", "Evento", "Recurso"};
-	public final String[][] conteudoLinhaDoTempo;
+	public String[][] conteudoLinhaDoTempo = new String[][]{{"-", "-", "-", "-"}};
 	
-	public TransportadoraDTO(){
-		referenciaTemporal = 0;
-		this.conteudoLinhaDoTempo = new String[][]{{"-", "-", "-", "-"}};
-	}
+	public int QUANTIDADE_CAMINHOES_FILA_CARREGAMENTO = 0;
+	public int TAMANHO_MINIMO_FILA_CARREGAMENTO = 0;
+	public int TAMANHO_MAXIMO_FILA_CARREGAMENTO = 0;
+	public double TAMANHO_MEDIO_FILA_CARREGAMENTO = 0;
 	
-	public TransportadoraDTO(int referenciaTemporal, String[][] conteudoLinhaDoTempo){
-		this.referenciaTemporal = referenciaTemporal;
-		this.conteudoLinhaDoTempo = conteudoLinhaDoTempo;
-	}
+	public int QUANTIDADE_CAMINHOES_FILA_PESAGEM = 0;
+	public int TAMANHO_MINIMO_FILA_PESAGEM = 0;
+	public int TAMANHO_MAXIMO_FILA_PESAGEM = 0;
+	public double TAMANHO_MEDIO_FILA_PESAGEM = 0;
+	
+	public int QUANTIDADE_CAMINHOS_ENTREGANDO = 0;
+	
+	public double TAXA_MEDIA_OCUPACAO_CARREGADOR = 0;
+	public double TAXA_MEDIA_OCUPACAO_BALANCA = 0;
+	
+	public int CONTADOR_VIAGENS = 0;
+	
+	public int TEMPO_MINIMO_ENTIDADE_FILA_CARREGADOR = 0;
+	public int TEMPO_MAXIMO_ENTIDADE_FILA_CARREGADOR = 0;
+	public int TEMPO_MEDIO_ENTIDADE_FILA_CARREGADOR = 0;
+	
+	public int TEMPO_MINIMO_ENTIDADE_FILA_PESAGEM = 0;
+	public int TEMPO_MAXIMO_ENTIDADE_FILA_PESAGEM = 0;
+	public int TEMPO_MEDIO_ENTIDADE_FILA_PESAGEM = 0;
+	
+	
+	public TransportadoraDTO(){ }
+
 }
